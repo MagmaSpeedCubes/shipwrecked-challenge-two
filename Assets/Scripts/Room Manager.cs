a9f10eee9b7e6c300a41dc7ee3aacc4fe09ebcbf
unsaltedkale <abuckley0278@student.pps.net>
2025-08-09 13:28:38 -0700

    making going between minigames work maybe

--- Assets/Scripts/Room Manager.cs
+++ Assets/Scripts/Room Manager.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using System.Collections;
+
 public class RoomManager : MonoBehaviour
 {
 
     RoomManager rm;
 
     public int currentMinigameIndex;
+
+    public Vector3 previousPlayerPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -21,19 +25,52 @@ public class RoomManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
         }
         
-        currentMinigameIndex = 0;
+        currentMinigameIndex = 1;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space"))
+        {
+            minigameWasWon();
+        }
         
     }
 
     public void startMiniGameButtonPressed() 
     {
-        SceneManager.LoadScene(currentMinigameIndex);
+        previousPlayerPosition = GameObject.Find("Player").transform.position;
+
+        SceneManager.LoadScene(currentMinigameIndex, LoadSceneMode.Single);
+
+    }
+
+    public void minigameWasWon()
+    {
+        currentMinigameIndex++;
+
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+
+        StartCoroutine(waitAFrame()); 
+
+    }
+
+    public IEnumerator waitAFrame()
+    {
+
+        yield return null;
+
+        GameObject.Find("Player").transform.position = previousPlayerPosition;
+    }
+
+    public void minigameWasLost()
+    {
+
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+
+
 
     }
 
